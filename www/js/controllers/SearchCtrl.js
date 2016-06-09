b9ae4d5040786f77e6694ca102830a8e9b96a9d8
Julien BRAT <bratjulien@gmail.com>
2016-06-09 10:58:11 +0200

    datepicker, timepicker, autocomplete input

---
+++ www/js/controllers/SearchCtrl.js
@@ -0,0 +1,4 @@
+angular.module('BlaBlaCar')
+    .controller('SearchCtrl', function($scope) {
+
+    });
--- www/js/controllers/TrajetCtrl.js
+++ www/js/controllers/TrajetCtrl.js
@@ -1,41 +1,51 @@
 angular.module('BlaBlaCar')
     .controller('TrajetCtrl', function($scope, ionicDatePicker, ionicTimePicker) {
 
-        // datePicker
-        var datePickerTrajet = {
+        //------------------- DatePicker ------------------//
+        var datePickerTrajetStart = {
             callback: function (val) {  //Mandatory
-                console.log('Return value from the datepicker popup is : ' + val, new Date(val));
+                $scope.dateDepart = new Date(val);
             },
-            disabledDates: [            //Optional
-                new Date(2016, 2, 16),
-                new Date(2015, 3, 16),
-                new Date(2015, 4, 16),
-                new Date(2015, 5, 16),
-                new Date('Wednesday, August 12, 2015'),
-                new Date("08-16-2016"),
-                new Date(1439676000000)
-            ],
-            from: new Date(2012, 1, 1), //Optional
-            to: new Date(2016, 10, 30), //Optional
-            inputDate: new Date(),      //Optional
-            mondayFirst: true,          //Optional
-            disableWeekdays: [0],       //Optional
-            closeOnSelect: false,       //Optional
-            templateType: 'popup'       //Optional
+            from: new Date(2012, 1, 1),
+            to: new Date(2016, 10, 30),
+            inputDate: new Date(),
+            mondayFirst: true,
+            disableWeekdays: [0],
+            closeOnSelect: false,
+            templateType: 'popup'
         };
 
-        $scope.openDatePicker = function(){
-            ionicDatePicker.openDatePicker(datePickerTrajet);
+        var datePickerTrajetEnd = {
+            callback: function(val) {
+                $scope.dateEnd = new Date(val);
+            },
+            from: new Date(2012, 1, 1),
+            to: new Date(2016, 10, 30),
+            inputDate: new Date(),
+            mondayFirst: true,
+            disableWeekdays: [0],
+            closeOnSelect: false,
+            templateType: 'popup'
+        }
+
+        $scope.openDatePickerStart = function(){
+            ionicDatePicker.openDatePicker(datePickerTrajetStart);
         };
 
-        // TimePicker
-        var timePickerTrajet = {
+        $scope.openDatePickerEnd = function(){
+            ionicDatePicker.openDatePicker(datePickerTrajetEnd);
+        };
+
+        //--------------------------------------------------------------------//
+
+        //--------------------------TimePicker ------------------------------//
+        var timePickerTrajetStart = {
             callback: function (val) {      //Mandatory
                 if (typeof (val) === 'undefined') {
-                    console.log('Time not selected');
+                    alert("Veuillez sélectionner l'heure");
                 } else {
                     var selectedTime = new Date(val * 1000);
-                    console.log('Selected epoch is : ', val, 'and the time is ', selectedTime.getUTCHours(), 'H :', selectedTime.getUTCMinutes(), 'M');
+                    $scope.timeDepart = selectedTime.getUTCHours()+":"+selectedTime.getUTCMinutes();
                 }
             },
             inputTime: 50400,   //Optional
@@ -43,7 +53,37 @@ angular.module('BlaBlaCar')
             step: 15,           //Optional
             setLabel: 'Set2'    //Optional
         };
-        $scope.openTimePicker = function() {
-            ionicTimePicker.openTimePicker(timePickerTrajet);
+
+        // TimePicker
+        var timePickerTrajetEnd = {
+            callback: function (val) {      //Mandatory
+                if (typeof (val) === 'undefined') {
+                    alert("Veuillez sélectionner l'heure");
+                } else {
+                    var selectedTime = new Date(val * 1000);
+                    $scope.timeEnd = selectedTime.getUTCHours()+":"+selectedTime.getUTCMinutes();
+                }
+            },
+
+            inputTime: 50400,   //Optional
+            format: 12,         //Optional
+            step: 15,           //Optional
+            setLabel: 'Set2'    //Optional
+        };
+
+        $scope.openTimePickerStart = function() {
+            ionicTimePicker.openTimePicker(timePickerTrajetStart);
         }
+
+        $scope.openTimePickerEnd = function() {
+            ionicTimePicker.openTimePicker(timePickerTrajetEnd);
+        }
+        //--------------------------------------------------------------------//
+
+
+        $scope.postTrajet = function() {
+            
+        }
+
+
     });
