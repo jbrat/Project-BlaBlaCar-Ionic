e8deea41fe76e016737fb59aa50eeca4a2696d07
Julien BRAT <bratjulien@gmail.com>
2016-06-15 17:40:24 +0200

    fin de la recherche, tout est OK

--- www/js/controllers/ResultsCtrl.js
+++ www/js/controllers/ResultsCtrl.js
@@ -5,30 +5,27 @@ angular.module('BlaBlaCar')
         var searchParams = angular.fromJson($stateParams.searchValue);
 
         var itemsRef = new Firebase("https://project-8473858751034565420.firebaseio.com/trajets");
-        
+
         if(!searchParams || searchParams === undefined) {
             $scope.results = $firebaseArray(itemsRef);
         } else {
-
-            var results = Array();
-
+            var results = [];
             var query = itemsRef.on('value', function (snapshot) {
                 var listTrajets = snapshot.val();
 
                 angular.forEach(listTrajets, function (trajet){
+
                     if (trajet.pointArrivePays == searchParams.arrivePays
                         &&trajet.pointArriveVille == searchParams.arriveVille
                         && trajet.pointDepartPays == searchParams.departPays
                         && trajet.pointDepartVille == searchParams.departVille
                         && trajet.dateDepart == searchParams.date) {
-
                         results.push(trajet);
                     }
                 })
             });
 
             $scope.results = results;
-         
         }
     });
 
