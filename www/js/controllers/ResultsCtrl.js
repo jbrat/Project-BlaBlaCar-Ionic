39ec591f61a1b10063a3fcff2094f34f09523820
BRAT Julien <Julien.Brat@etu.udamail.fr>
2016-06-15 10:35:50 +0200

    result up to date

--- www/js/controllers/ResultsCtrl.js
+++ www/js/controllers/ResultsCtrl.js
@@ -5,6 +5,9 @@ angular.module('BlaBlaCar')
         var searchParams = angular.fromJson($stateParams.searchValue);
 
         var itemsRef = new Firebase("https://project-8473858751034565420.firebaseio.com/trajets");
+        
+// TO DELETE        
+searchParams = null;
 
         if(!searchParams || searchParams === undefined) {
             $scope.results = $firebaseArray(itemsRef);
